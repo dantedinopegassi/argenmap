06bf475953040a14db8482c35b357de12c55bc44
Interno <internuser04@gmail.com>
2024-07-03 12:33:56 -0300

    importando analytics

--- srcTSS/script.sh
+++ srcTSS/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find ./js/plugins/ -type f -name '*.ts' | while read -r file; do
+find ./js/map/plugins/ -type f -name '*.ts' | while read -r file; do
 	mv "$file" "${file%.*}.js"
 done
